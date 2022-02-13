3b53dfbffee3916f91f0628dec91ed0028902e03
Dkboss743 <2019246@iiitdmj.ac.in>
2022-02-13 18:38:58 +0530

    changes

--- script.js
+++ script.js
@@ -61,9 +61,10 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
   containerMovements.innerHTML = "";
-  movements.forEach(function (mov, i) {
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
       <div class="movements__row">
@@ -77,4 +78,106 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
-displayMovements(account1.movements);
+const displaydata = function (user) {
+  displayMovements(user.movements);
+  calcDisplaySumm(user);
+  calcPrintBalance(user);
+};
+const calcPrintBalance = function (account) {
+  const balance = account.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+  account.balance = balance;
+};
+const calcDisplaySumm = function (accounts) {
+  const incomes = accounts.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}$`;
+  const outcome = accounts.movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = outcome;
+  const interest = accounts.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * accounts.interestRate) / 100)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = interest.toPrecision(4);
+};
+
+const createUsernames = function (accounts) {
+  accounts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((val) => val[0])
+      .join("");
+  });
+};
+let currentAccount;
+createUsernames(accounts);
+btnLogin.addEventListener("click", function (E) {
+  E.preventDefault();
+
+  const user = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  if (user && Number(inputLoginPin.value) === user.pin) {
+    labelWelcome.textContent = `Welcome back, ${user.owner.split(" ")[0]}`;
+    currentAccount = user;
+    inputLoginUsername.value = "";
+    inputLoginPin.value = " ";
+    inputLoginPin.blur();
+    containerApp.style.opacity = 100;
+    displaydata(user);
+  } else {
+    containerApp.style.opacity = 0;
+  }
+});
+btnTransfer.addEventListener("click", function (E) {
+  E.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const tranuser = accounts.find((acc) => acc.owner === inputTransferTo.value);
+  inputTransferAmount.value = inputTransferTo.value = "";
+  console.log(amount, tranuser);
+  if (
+    amount > 0 &&
+    tranuser &&
+    currentAccount.balance >= amount &&
+    tranuser.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    tranuser.movements.push(amount);
+    displaydata(currentAccount);
+  }
+});
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+
+    displaydata(currentAccount);
+  }
+});
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    inputCloseUsername.value = inputClosePin.value = "";
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+});
+let val = false;
+btnSort.addEventListener("click", function (e) {
+  val = !val;
+  displayMovements(currentAccount.movements, val);
+});
